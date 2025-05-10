79e87baa94cd4e803fe391e1bb58d36ceaae4288
RainerMorales <jerenmae212212@gmail.com>
2025-05-10 22:01:14 +0800

    add firebase

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,11 @@
 import { ModeToggle } from "./components/mode-toggle";
 
-
-function Header(){
-    return (
-      <header className="bg-black fixed p-6 top-0 shadow-md  w-full flex justify-between items-center">
-        <div className="text-2xl text-white font-bold">📑 TODO-LIST APP</div>
-        <ModeToggle></ModeToggle>
-      </header>
-    );
+function Header() {
+  return (
+    <header className="bg-black fixed p-6 top-0 shadow-md  w-full flex justify-between items-center">
+      <div className="text-2xl text-white font-bold">📑 Task-Manager</div>
+      <ModeToggle></ModeToggle>
+    </header>
+  );
 }
-export default Header
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optionalmk
+const firebaseConfig = {
+  apiKey: "AIzaSyAMyskPXguh94JTYe4jR6W5KvhUY0jCHuQ",
+  authDomain: "todo-app-6ef4a.firebaseapp.com",
+  projectId: "todo-app-6ef4a",
+  storageBucket: "todo-app-6ef4a.firebasestorage.app",
+  messagingSenderId: "130405544373",
+  appId: "1:130405544373:web:f06027b1aea4a7251129fc",
+  measurementId: "G-E1K3ZRL7B7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
